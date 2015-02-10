158a9f3c82f8e39a5e45253c356f90119c1ec809
Priya14 <shanmugapriya141292@gmail.com>
2015-02-10 16:10:23 +0530

    added clean code matters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,3 +35,12 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+// $(function() {
+//     var $list = $('ul li');
+//     $list.filter(':first').addClass('animated flipInX');
+//     setInterval(function() {
+// 	if( $list.filter('.flipInX').index() !== $list.length) {
+// 	    $list.filter('.flipInX').removeClass('animated flipInX').next().addClass('animated flipInX');
+// 	}
+//     }, 400);
+// });
